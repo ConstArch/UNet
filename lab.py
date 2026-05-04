7c56b859c07919e2900b92d43f8459261fed9b0a
ConstArch <90158169+ConstArch@users.noreply.github.com>
2026-05-04 21:23:30 +0300

    Add files via upload

--- lab.py
+++ lab.py
@@ -132,14 +132,15 @@ class AllMetricsApplier(nt.AbstractLossApplier):
         
         targets_new_shape = (targets.shape[0], 1, targets.shape[1], targets.shape[2])
         targets_one_hot = monai.networks.one_hot(targets.reshape(targets_new_shape), num_classes=3)
+        targets_one_hot_cpu = targets_one_hot.cpu()
         
         return {
             'SoftIoU'          : 1 - self.miou_loss(output, targets_one_hot),
             'SoftDice'         : 1 - self.dice_loss(output, targets_one_hot),
-            'HausdorffDTLoss'  : self.haus_loss(output, targets_one_hot),
+            'HausdorffDTLoss'  : self.haus_loss(output.cpu(), targets_one_hot_cpu),
             'IoU'              : self.miou_metric(outputs_one_hot, targets_one_hot).mean(),
             'Dice'             : self.dice_metric(outputs_one_hot, targets_one_hot).mean(),
-            'HausdorffDistance': self.haus_metric(outputs_one_hot, targets_one_hot).mean()
+            'HausdorffDistance': self.haus_metric(outputs_one_hot.cpu(), targets_one_hot_cpu).mean()
         }
     
     def to_dataloader(self, net, dataloader):
@@ -269,13 +270,13 @@ def main():
         trainval_dataset,
         batch_size=100,
         shuffle=True,
-        pin_memory=True
+        #pin_memory=True
     )
     
     test_dataloader = torch.utils.data.DataLoader(
         test_dataset,
         batch_size=100,
-        pin_memory=True
+        #pin_memory=True
     )
     
     epoch_count = 10
